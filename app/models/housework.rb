fff1fd63c1a668a5370a5060eb57e6b83b88c1ac
ikumi-taka <canomori0311@gmail.com>
2022-12-02 09:56:40 +0900

    アソシエーションの記述

--- app/models/housework.rb
+++ app/models/housework.rb
@@ -1,2 +1,4 @@
 class Housework < ApplicationRecord
+  has_many :housework_users
+  has_many :users, through: :housework_users
 end
--- app/models/housework_user.rb
+++ app/models/housework_user.rb
@@ -1,2 +1,4 @@
 class HouseworkUser < ApplicationRecord
+  belongs_to :user
+  belongs_to :housework
 end
