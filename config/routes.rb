b98d45ab6e81c3e3f4c742c082ca5b371defbbd7
ikumi-taka <canomori0311@gmail.com>
2022-12-05 13:36:43 +0900

    ルーティングエラー解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'houseworks', to: 'houseworks#index'
   root to: 'houseworks#index'
   resources :users, only: [:edit, :update]
   resources :houseworks, only: [:new, :create]
