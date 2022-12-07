8867182c2cde09d9bdeca6d47b9aa414ee223f5c
ikumi-taka <canomori0311@gmail.com>
2022-12-07 21:06:34 +0900

    createアクション実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,18 @@
 class RecordsController < ApplicationController
   def index
+    @record = Record.new
+    @housework = Housework.find(params[:housework_id])
+  end
+
+  def create
+    @housework = Housework.find(params[:housework_id])
+    @record = @housework.records.new(record_params)
+    @record.save
+  end
+
+  private
+
+  def record_params
+    params.require(:record).permit(:content).merge(user_id: current_user.id)
   end
 end
