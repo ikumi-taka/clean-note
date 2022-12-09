907967b651d6c5c5c86974aa94f8aaa19a5501f6
ikumi-taka <canomori0311@gmail.com>
2022-12-09 12:52:12 +0900

    画像表示の実装

--- app/models/housework.rb
+++ app/models/housework.rb
@@ -2,7 +2,7 @@ class Housework < ApplicationRecord
   has_many :housework_users, dependent: :destroy
   has_many :users, through: :housework_users
   has_many :records, dependent: :destroy
-  has_one_attacher :image
+  has_one_attached :image
 
   validates :housework_name, presence: true
   #validates :name, presence: true
