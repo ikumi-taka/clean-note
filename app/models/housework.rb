85352811e2cb99235950e95b7b769f96ca2c960d
ikumi-taka <canomori0311@gmail.com>
2022-12-09 12:30:08 +0900

    CSS修正

--- app/models/housework.rb
+++ app/models/housework.rb
@@ -2,6 +2,7 @@ class Housework < ApplicationRecord
   has_many :housework_users, dependent: :destroy
   has_many :users, through: :housework_users
   has_many :records, dependent: :destroy
+  has_one_attacher :image
 
   validates :housework_name, presence: true
   #validates :name, presence: true
