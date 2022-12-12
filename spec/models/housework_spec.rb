b5f260e12e6043850ce03587e3c13546082dcb50
ikumi-taka <canomori0311@gmail.com>
2022-12-12 21:48:42 +0900

    houseworkモデルの単体テストコードの実装

--- spec/models/housework_spec.rb
+++ spec/models/housework_spec.rb
@@ -1,4 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Housework, type: :model do
+  before do
+    @housework = FactoryBot.build(:housework)
+  end
+
+  describe '掃除記録ページの作成' do
+    context '掃除記録ページの作成ができるとき'do
+      it 'housework_nameとimageが存在していれば登録ができる' do
+        expect(@housework).to be_valid
+      end
+    end
+    context '掃除記録ページの作成ができないとき'do
+      it 'housework_nameが空だと登録できない' do
+        @housework.housework_name = ''
+        @housework.valid?
+        expect(@housework.errors.full_messages).to include("Housework name can't be blank")
+      end
+      it 'imageが空だと登録できない' do
+        @housework.image = nil
+        @housework.valid?
+        expect(@housework.errors.full_messages).to include("Image can't be blank")
+      end
+    end
+  end
 end
+
