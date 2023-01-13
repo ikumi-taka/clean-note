fc28c70604c0640b280737274bd720b7348849da
ikumi-taka <canomori0311@gmail.com>
2023-01-13 21:50:58 +0900

    コントローラーアクション定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,17 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :record_params, only: [:create, :destroy]
+  def create
+    Like.create(user_id: current_user.id, record_id: @record.id)
+  end
+
+  def destroy
+    like = Like.find_by(user_id: current_user.id, record_id: @record.id)
+    like.destroy
+  end
+
+  private
+  def record_params
+    @record = Record.find(params[:record_id])
+  end
 end
