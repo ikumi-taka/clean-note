46a8eed92f4fe4630bb6939fcbe02d787e36c0a2
ikumi-taka <canomori0311@gmail.com>
2023-01-13 20:54:48 +0900

    ルーティング設定、likeモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root to: 'houseworks#index'
   resources :users, only: [:edit, :update]
   resources :houseworks, only: [:new, :create, :destroy] do
-    resources :records, only: [:index, :create]
+    resources :records, only: [:index, :create] do
+      resources :like, only: [:create, :destroy]
+    end
   end
 
 end
