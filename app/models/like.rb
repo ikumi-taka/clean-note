8899481e9db66bae7ae6ab01c252e03743af235e
ikumi-taka <canomori0311@gmail.com>
2023-01-13 21:23:39 +0900

    バリデーション設定

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :record
   belongs_to :user
+
+  # record_idとuser_idの組は1組だけ
+  validates_uniqueness_of :record_id, scope: :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,9 @@ class User < ApplicationRecord
   has_many :records
   has_many :like, dependent: :destroy
   has_many :like_records, through: :likes, sorce: :record
+
+  # likesテーブルにrecord_idが存在しているかどうか検索をかける
+  def liked_by?(record_id)
+    likes.where(record_id: recors_id).exists?
+  end
 end
