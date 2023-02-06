1f48d9979e5c5e99971034e37a9982e644ceaffb
ikumi <111064442+ikumi-taka@users.noreply.github.com>
2023-02-06 22:41:37 +0900

    Revert "いいね機能実装"

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  # get 'houseworks', to: 'houseworks#index'
   root to: 'houseworks#index'
   resources :users, only: [:edit, :update]
   resources :houseworks, only: [:new, :create, :destroy] do
     resources :records, only: [:index, :create]
+      # resources :likes, only: [:create, :destroy]
   end
     post 'like/:id' => 'likes#create', as: 'create_like'
     delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
